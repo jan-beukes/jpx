4e1b97d427acea17829f50a168b0eee9a65cebb4
Jan Beukes <beukes.janadam@gmail.com>
2025-03-31 18:15:30 +0200

    build script

--- build_web.sh
+++ build_web.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -eu
 
 # Point this to where you installed emscripten. Optional on systems that already
-# have `emcc` in the path.
-EMSCRIPTEN_SDK_DIR="$HOME/repos/emsdk"
-OUT_DIR="build/web"
+EMSCRIPTEN_SDK_DIR="$HOME/Software/emsdk"
+OUT_DIR="web_build"
 
 mkdir -p $OUT_DIR
 
@@ -16,22 +15,20 @@ export EMSDK_QUIET=1
 # The emcc call will be fed the actual raylib library file. That stuff will end
 # up in env.o
 #
-# Note that there is a rayGUI equivalent: -define:RAYGUI_WASM_LIB=env.o
-odin build source/main_web -target:js_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -define:RAYGUI_WASM_LIB=env.o -vet -strict-style -out:$OUT_DIR/game
+odin build src/main_web -target:js_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -out:$OUT_DIR/jpx -debug
 
 ODIN_PATH=$(odin root)
 
 cp $ODIN_PATH/core/sys/wasm/js/odin.js $OUT_DIR
+cp src/main_web/jpx.js $OUT_DIR
 
-files="$OUT_DIR/game.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}/vendor/raylib/wasm/libraygui.a"
+files="$OUT_DIR/jpx.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a"
 
-# index_template.html contains the javascript code that calls the procedures in
-# source/main_web/main_web.odin
-flags="-sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sASSERTIONS --shell-file source/main_web/index_template.html --preload-file assets"
+flags="-sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sASSERTIONS --shell-file src/main_web/index_template.html"
 
-# For debugging: Add `-g` to `emcc` (gives better error callstack in chrome)
+# For debugging: Add `-g` to `emcc`
 emcc -o $OUT_DIR/index.html $files $flags
 
-rm $OUT_DIR/game.wasm.o
+rm $OUT_DIR/jpx.wasm.o
 
 echo "Web build created in ${OUT_DIR}"
